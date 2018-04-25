8495e40120cd0477ddfec19a7d2bee070090b25b
Aanchal Sharma <aanchal1103@gmail.com>
2018-04-25 22:09:18 +0530

    create_react_app structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Table } from 'components';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Table />
+      </div>
+    );
+  }
+}
+
+export default App;
